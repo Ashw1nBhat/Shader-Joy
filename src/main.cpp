f553f0803a0d61609f17cef68bd32e9867696466
ashwin_bhat <ashwinbhat31@gmail.com>
2025-06-17 16:18:52 +0530

    Add triangle data

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,12 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+const float TRIANGLE_DATA[] = {
+	0, 1, 0, 1, 0, 0,
+	-1, -1, 0, 0, 1, 0,
+	1, -1, 0, 0, 0, 1
+};
+
 int main() {
 	if (!glfwInit()) {
 		std::cout << "GLFW initialization failed" << "\n";
@@ -21,12 +27,20 @@ int main() {
 		return 1;
 	}
 
+	GLuint buffer = 0;
+	glGenBuffers(1, &buffer);
+
+	glBindBuffer(GL_ARRAY_BUFFER, buffer);
+	glBufferData(buffer, sizeof(TRIANGLE_DATA), TRIANGLE_DATA, GL_STATIC_DRAW);
+
 	while (!glfwWindowShouldClose(window)) {
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
+
+	glDeleteBuffers(1, &buffer);
 
 	glfwDestroyWindow(window);
 	glfwTerminate();
